0feddebec751db51ffcdbb146588f20810e6311d
Sebastien Kempf <56154464+sebkpf@users.noreply.github.com>
2023-01-27 21:07:13 +0100

    send email on new offers

---
+++ email.js
@@ -0,0 +1,43 @@
+const nodemailer = require("nodemailer");
+const { gmailAddress, gmailAppPassword } = require("./config.json");
+
+const client = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: gmailAddress,
+    pass: gmailAppPassword,
+  },
+});
+
+const sendMail = async (offers) => {
+  const emailBody = offers.map((offer) => {
+    return `
+      Property: ${offer.property}
+      Realt price: ${offer.realt_price}
+      Offer price: ${offer.offer_price}
+      Offer amount: ${offer.offer_amount}
+      Price difference: ${offer.price_difference}
+      Current yield: ${offer.current_yield}
+      New yield: ${offer.new_yield}
+      Rent start date: ${offer.rent_start_date}
+    `;
+  });
+  console.log("🚀 ~ file: email.js:25 ~ emailBody ~ emailBody", emailBody);
+
+  const mailOptions = {
+    from: "realt@gmail.com",
+    to: gmailAddress,
+    subject: "Realt RMM - New offer",
+    text: emailBody.toString(),
+  };
+
+  client.sendMail(mailOptions, (err) => {
+    if (err) {
+      console.log("Error: ", err);
+    } else {
+      console.log("Email sent!");
+    }
+  });
+};
+
+module.exports = { sendMail };
--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 // const puppeteer = require("puppeteer");
 const puppeteer = require("puppeteer-core");
-const sqlite3 = require("sqlite3").verbose();
-const cron = require("node-cron");
 const realtokens = require("./realtokens.json");
+const {
+  addNewProperty,
+  createPropertiesTable,
+  getProperty,
+} = require("./database");
+const { sendMail } = require("./email");
+const { url } = require("./config.json");
 
-let db = new sqlite3.Database("./yam.db");
-
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
-const { channelId, token, url } = require("./config.json");
-
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-// Log in to Discord with your client's token
-client.login(token);
+createPropertiesTable();
 
 async function scrapeWebsite() {
   const browser = await puppeteer.launch({
@@ -32,17 +21,36 @@ async function scrapeWebsite() {
     userDataDir: "./myChromeSession",
   });
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(url, { waitUntil: "networkidle2" });
 
-  // Extract the necessary information from the website
   const data = await page.evaluate(() => {
-    // Code to extract the data, for example:
-    const elements = document.querySelectorAll("#tableau tr td:nth-child(2)");
-    const data = [];
-    elements.forEach((el) => {
-      data.push(el.textContent);
-    });
-    return data;
+    const offerProps = [
+      "offer",
+      "property",
+      "realt_price",
+      "offer_price",
+      "offer_amount",
+      "price_difference",
+      "current_yield",
+      "new_yield",
+      "rent_start_date",
+    ];
+
+    const table = document.getElementById("tableau");
+    const offers = [];
+
+    for (let i = 0; i < table.rows.length; i++) {
+      const offer = {};
+
+      for (let j = 0; j < table.rows[i].cells.length; j++) {
+        const cellText = table.rows[i].cells[j].textContent;
+        offer[offerProps[j]] = cellText;
+      }
+
+      offers.push(offer);
+    }
+
+    return offers;
   });
 
   browser.close();
@@ -51,24 +59,21 @@ async function scrapeWebsite() {
 
 async function checkForMatches() {
   const data = await scrapeWebsite();
+  const offers = [];
 
   for (let i = 0; i < data.length; i++) {
-    if (realtokens.includes(data[i])) {
-      sendNotification(data[i]);
-      break;
+    if (realtokens.includes(data[i].property)) {
+      const offer = getProperty(data[i].offer);
+      if (!offer) {
+        offers.push(data[i]);
+        addNewProperty(data[i]);
+      }
     }
+  }
+
+  if (offers.length > 0) {
+    sendMail(offers);
   }
 }
 
-function sendNotification(match) {
-  client.on("ready", () => {
-    const channel = client.channels.cache.get(channelId);
-    channel.send(`A match was found: ${match}`);
-  });
-}
-
-// create a properties table if it doesn't exist with the following columns: offer, property,	realt_price, offer_price,	offer_amount, current_yield, rent_start_date
-
-cron.schedule("*/15 * * * *", () => {
-  checkForMatches();
-});
+checkForMatches();
